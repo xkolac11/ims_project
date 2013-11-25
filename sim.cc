824f6e800f8ac48294c0688e7e3350206d5c100c
xkolac11 <xkolac11@stud.fit.vutbr.cz>
2013-11-26 00:06:43 +0100

    Bloky, Integrator, metody

--- sim.cc
+++ sim.cc
@@ -6,7 +6,6 @@ bool out_to_close = false;
 using namespace std;
 
 void SetOut(const char *fd){
-
     if(fd != NULL){
         out = fopen(fd, "w+");
         if(out == NULL){
@@ -20,9 +19,98 @@ void SetOut(const char *fd){
        //stdout
        out = stdout;
     }
-
 }
 
 void Print(const char *str){
 	fprintf(out, "%s", str);
+}
+
+/** IMPLEMENTATIONS OF CLASSES METHODS **/
+
+/** Input method **/
+double Input::Val() const{
+    return bp->Val();
+}
+
+/** Var methods **/
+double Var::Val(){
+    return i.Val();
+}
+
+void Var::Set(Input in){
+    i = in;
+}
+
+/** Const method **/
+double Const::Val(){
+    return val;
+}
+
+/** Integrator methods */
+void Integrator::Init(){
+    output_val = init_val;
+}
+
+void Integrator::Set(Input in, double in_val){
+    i = in;
+    init_val = in_val;
+}
+
+void Integrator::Set(Integrator &integrator, double in_val){
+    i = integrator;
+    init_val = in_val;
+}
+
+double Integrator::Get(){
+    return input_val;
+}
+
+double Integrator::Val(){
+    return output_val;
+}
+
+double Integrator::IVal(){
+    return i.Val();
+}
+
+void Integrator::EVal(){
+    input_val = i.Val();
+}
+
+void Integrator::SetVal(double val){
+    output_val = val;
+}
+
+/** Operation blocks (OpBlock1/2) methods **/
+double OpBlock1::iVal(){
+    return in.Val();
+}
+
+double OpBlock2::iVal1(){
+    return in1.Val();
+}
+
+double OpBlock2::iVal2(){
+    return in2.Val();
+}
+
+/** Arithmetic blocks methods **/
+double UMin::Val(){
+    return -iVal();
+}
+
+double Add::Val(){
+    return iVal1() + iVal2();
+}
+
+double Sub::Val(){
+    return iVal1() - iVal2();
+}
+
+double Mul::Val(){
+    return iVal1() * iVal2();
+}
+
+double Div::Val(){
+    return iVal1() / iVal2();
 }
--- sim.hh
+++ sim.hh
@@ -1,5 +1,5 @@
-#ifndef SIM_H
-#define SIM_H
+#ifndef SIM_HH
+#define SIM_HH
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -14,8 +14,140 @@ using namespace std;
 extern FILE *out;
 extern bool out_to_close;
 
-void SetOutput(const char *file);
+void SetOut(const char *file);
 void Print(const char *str);
 
+/** class prototypes **/
+class Block;
+class Input;
+class Var;
+class Const;
+class OpBlock1;
+class OpBlock2;
+class Add;
+class Sub;
+class Mul;
+class Div;
+class UMin;
+class Integrator;
+class Sim;
+
+/** base class for all blocks
+    every block has it's own output value */
+class Block{
+    public:
+        virtual double Val() = 0;
+};
+
+/** wrapper for pointer to objects of a Block
+    used for referencing of continuous blocks in expressions */
+class Input{
+    Block *bp;
+    public:
+        Input(const Input &i): bp(i.bp){}
+        Input(Block &b): bp(&b){}
+        Input(Block *b): bp(b){}
+        double Val() const;
+};
+
+/** block representating variable
+    value can be changed **/
+class Var : public Block{
+    Input i;
+    public:
+        Var() : i(0) {}
+        void Set(Input in);
+        virtual double Val();
+};
+
+/** block representating constant
+    constant keeps always the same (defined) value **/
+class Const : public Block{
+    const double val;
+    public:
+        Const(double v) : val(v){}
+        virtual double Val();
+};
+
+/** block representating operations with one operand **/
+class OpBlock1 : public Block{
+    Input in;
+    public:
+        OpBlock1(Input i);
+        double iVal();
+};
+
+/** block representating operations with two operands **/
+class OpBlock2 : public Block{
+    Input in1, in2;
+    public:
+        OpBlock2(Input i1, Input i2);
+        double iVal1();
+        double iVal2();
+};
+
+
+/** operation unary minus **/
+class UMin : public OpBlock1{
+    public:
+        UMin(Input i) : OpBlock1(i){}
+        virtual double Val();
+};
+
+/** operation add **/
+class Add : public OpBlock2{
+    public:
+        Add(Input i1, Input i2) : OpBlock2(i1, i2){}
+        virtual double Val();
+};
+
+/** operation subtract **/
+class Sub : public OpBlock2{
+    public:
+        Sub(Input i1, Input i2) : OpBlock2(i1, i2){}
+        virtual double Val();
+};
+
+/** operation multiply **/
+class Mul : public OpBlock2{
+    public:
+        Mul(Input i1, Input i2) : OpBlock2(i1, i2){}
+        virtual double Val();
+};
+
+/** operation divide **/
+class Div : public OpBlock2{
+    public:
+        Div(Input i1, Input i2) : OpBlock2(i1, i2){}
+        virtual double Val();
+};
+
+/** overloading of operators, so they can be used with ooprands like
+    instance of Input class **/
+Input operator + (Input a, input b);
+Input operator - (Input a, input b);
+Input operator * (Input a, input b);
+Input operator / (Input a, input b);
+Input operator - (Input a);
+
+/** block integrator, represents numeric integration **/
+class Integrator : public Block{
+    public:
+        double input_val;
+        double output_val;
+    protected:
+        Input i;
+        double init_val;
+    private:
+        Integrator(): i(0), init_val(0.0) {}
+        void Init();
+        void Set(Input in, double in_val = 0);
+        void Set(Integrator &integrator, double in_val = 0);
+        double Get();
+        double Val();
+        double IVal();
+        void EVal();
+        void SetVal(double val);
+};
 
 #endif
